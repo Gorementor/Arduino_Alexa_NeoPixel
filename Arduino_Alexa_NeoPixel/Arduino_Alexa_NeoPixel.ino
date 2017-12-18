8d77b143a3e3a89aae4da6e6a28a4dcd090d64b2
Sven <Gorementor@users.noreply.github.com>
2017-12-18 15:41:58 +0100

    Add project files.

---
+++ Arduino_Alexa_NeoPixel/Arduino_Alexa_NeoPixel.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_Alexa_NeoPixel.ino
+ Created:	12/18/2017 3:41:54 PM
+ Author:	Sven
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
